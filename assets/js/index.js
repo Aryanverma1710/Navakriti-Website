956dd29811e4c662a4b606055a585f249af2ddbb
Aryanverma1710 <expertakv03@gmail.com>
2023-07-23 04:13:42 +0530

    Added new footer and section for customer reviews

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  // Smooth scrolling for navigation links
+  $("a.nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1000 // Adjust the animation speed as desired
+      );
+    }
+  });
+
+  // Show or hide the back-to-top button based on the scroll position
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top-btn').fadeIn();
+    } else {
+      $('#back-to-top-btn').fadeOut();
+    }
+  });
+
+  // Smooth scroll to the top when the button is clicked
+  $('#back-to-top-btn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    return false;
+  });
+});
